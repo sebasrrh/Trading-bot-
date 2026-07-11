ed084919a9fcc73636ec09a5d3920efda43e65e6
madpods <=>
2026-07-10 18:29:41 -0700

    test that the github action works

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -4,4 +4,4 @@ import { serve } from '@hono/node-server';
 const port = 8787;
 console.log(`api running on http://localhost:${port}`);
 
-serve({ fetch: app.fetch, port });
+serve({ fetch: app.fetch, port, hostname: '0.0.0.0' });
